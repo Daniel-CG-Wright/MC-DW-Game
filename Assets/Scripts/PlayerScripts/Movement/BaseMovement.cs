4b05c570f917d413d76c1aa3bd145df2a81c7610
Daniel Wright <danielcgwright@gmail.com>
2022-06-22 14:20:08 +0100

    crouching WIP

--- Assets/Scripts/PlayerScripts/Movement/BaseMovement.cs
+++ Assets/Scripts/PlayerScripts/Movement/BaseMovement.cs
@@ -7,19 +7,59 @@ public class BaseMovement : MonoBehaviour
 
     public CharacterController controller;
     public float moveSpeed = 10f; //Stores a movespeed multiplier, can be changed for sprinting etc.
-    
+    public float gravity = -9.81f;
+
+    public Transform groundCheck;
+    public float groundDistance = 0.2f;
+    public LayerMask groundMask;
+
+    bool isGrounded;
+    Vector3 velocity;
+
+    [SerializeField] private float crouchHeight = 0.5f;
+    [SerializeField] private float standingHeight = 0.9f;
+    [SerializeField] private float timeToCrouch = 0.2f;
+    [SerializeField] private Vector3 crouchingCenter = new Vector3(0, 0.5f, 0);
+    [SerializeField] private Vector3 standingCenter = new Vector3(0, 0, 0);
+
+    private bool isCrouching;
+    private bool duringCrouchAnimation;
 
     // Update is called once per frame
     void Update()
     {
+        isGrounded = Physics.CheckSphere(groundCheck.position, groundDistance, groundMask);
+
         float x = Input.GetAxis("Horizontal");
         float z = Input.GetAxis("Vertical");
 
-        Vector3 moveVector =  transform.right * x + transform.forward * z; //Direction player is moving relative to actual player.
+        KeyboardMove(x, z);
 
-        controller.Move(moveSpeed * Time.deltaTime * moveVector); //Should move character
+        gravityCheck();
+
+        if (isGrounded && velocity.y < 0)
+        {
+            velocity.y = -1f;
+        }
 
     }
 
+    void KeyboardMove(float x, float z)
+    {
+        Vector3 moveVector = transform.right * x + transform.forward * z; //Direction player is moving relative to actual player.
+
+        controller.Move(moveSpeed * Time.deltaTime * moveVector); //Should move character
+
+        
+    }
+
+    void gravityCheck()
+    {
+
+        velocity.y += gravity * Time.deltaTime;
+
+        controller.Move(velocity * Time.deltaTime);
+
+    }
     
 }
